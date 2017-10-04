1fb8f17a25a002155f0d570cda38fabc864324b8
efekta <efekta9@gmail.com>
2017-10-04 16:47:57 +0300

    no message

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('.main-arrow').on('click', function(){
     $('html, body').animate({
         scrollTop: $("#down").offset().top
-    }, 2000);
+    }, 800);
     return false;
   });
 });
